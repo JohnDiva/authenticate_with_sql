1791ab984fa9a5184dc965a2942d469ededcffd2
JohnDiva <77943178+JohnDiva@users.noreply.github.com>
2021-04-06 23:08:05 +1000

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,28 +1,39 @@
-/*
-utilities file
-(i didn't want to clutter the main app.js)
-*/
 const {User} = require('./fake_models.js')
 
 async function matchCredentials(requestBody) { 
-    try{
-        const user = await User.findAll({
-        where: {
-            username: requestBody.username,
-            password: requestBody.password
-        }
-        
-    })
-    if (requestBody.username === user.username                   //undefined 
-        && requestBody.password === user.password) {
-            return true 
-        } else { 
-            return false 
-        } 
-     } catch(err){
-         alert(err)
-
-     }                                        
+    const user = await User.findOne({
+    where: {
+        username: requestBody.username
     }
+    
+});
+try{
+var username = user.username;
+}
+catch(err){
+    return false
+}
 
-    module.exports = matchCredentials
+const _user = await User.findOne({
+    where: {
+        password: requestBody.password
+    }
+    
+});
+try{
+var password = _user.password;
+}
+catch(err){
+    return false
+}
+
+if (requestBody.username === username             
+    && requestBody.password === password) {
+        return  true;
+    } else { 
+        return  false;
+    }
+}
+    
+
+module.exports = matchCredentials
